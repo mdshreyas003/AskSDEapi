34a79844ac577662a2fa896a5b8e7baf7392dfd6
mdshreyas <mdshreyas003@outlook.com>
2023-06-27 12:40:15 +0530

    cors error fixed

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const ProblemSet = require('./ProblemSet.json')
 const app = express()
-
+const cors = require('cors')
+app.use(cors({
+    origin:"http://localhost:3000"
+}))
 app.get('/api/problemset/',(req,res)=>{
     res.json(ProblemSet)
 })
