da96e8c7082c825b5d780aa8d997ec47825f123b
mdshreyas <mdshreyas003@outlook.com>
2023-07-02 09:09:40 +0530

    Scroll Api

--- app.js
+++ app.js
@@ -2,11 +2,25 @@ const express = require('express')
 const ProblemSet = require('./ProblemSet.json')
 const app = express()
 const cors = require('cors')
+const coins =  require('./coins.json')
+const size = coins.length
 app.use(cors({
     origin:"*"
 }))
 app.get('/api/problemset/',(req,res)=>{
     res.json(ProblemSet)
+})
+
+app.get('/api/getCoins/',(req,res)=>{
+    const per_page = req.query?.per_page
+    const page_no = req.query?.page_no
+    const data = new Array()
+    console.log(typeof(coins))
+    for(let i=per_page*(page_no-1);i<per_page*(page_no);i = (i+1)%size)
+    {
+        data.push(coins[i])
+    }
+    res.json(data)
 })
 
 app.listen(5000,()=>{
